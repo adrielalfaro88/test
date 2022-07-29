0518512de9bded1f4582e9d1e3f97db0cb4909fa
Adriel <adrielalfaro88@gmail.com>
2022-07-29 11:27:41 -0300

    este es mi cambio en la rama 3

--- test1.ts
+++ test1.ts
@@ -1,6 +1,7 @@
 alert("este es mi cambio 1");
 
 console.log("RAMA 2 2 cambio");
+console.log("RAMA 3 cambio 1");
 
 //este es un comentario
 
@@ -9,3 +10,4 @@ alert ("Este es mi cambio 2");
 console.log("primer cambio en rama1");
 
 console.log("RAMA 2 1 cambio");
+console.log("RAMA 3 cambio 2");
