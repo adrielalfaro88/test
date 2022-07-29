6715031b09d183f14fcabec8a392272c938cf9fa
Adriel <adrielalfaro88@gmail.com>
2022-07-29 11:26:31 -0300

    cambios en rama 2

--- test1.ts
+++ test1.ts
@@ -1,7 +1,11 @@
 alert("este es mi cambio 1");
 
+console.log("RAMA 2 2 cambio");
+
 //este es un comentario
 
 alert ("Este es mi cambio 2");
 
 console.log("primer cambio en rama1");
+
+console.log("RAMA 2 1 cambio");
