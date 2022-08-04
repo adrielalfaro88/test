458a223ef40b05d2d1e8c8b347c6d946de23810b
Adriel <adrielalfaro88@gmail.com>
2022-08-04 12:32:02 -0300

    Este es mi primer commmit con ammend

--- test1.ts
+++ test1.ts
@@ -4,8 +4,10 @@ console.log("RAMA 2 2 cambio");
 console.log("RAMA 3 cambio 1");
 
 //este es un comentario
+//este es otro comentario
+//este es otro comentario para el ammend
 
-alert ("Este es mi cambio 2");
+alert("Este es mi cambio 2");
 
 console.log("primer cambio en rama1");
 
